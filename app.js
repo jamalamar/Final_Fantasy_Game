83f9a30b8147ee6cdf1f658b52bc5ce8ac82d40d
Jamal Amar Bahena <jamal@Jamals-MacBook-Pro.local>
2019-01-09 10:56:20 -0600

    working combat functions for boss and character, added hp display on screen that updates every 100ms

--- app.js
+++ app.js
@@ -55,26 +55,44 @@ class enemy {
 	}
 
 	heal() {
+		if(this.hp<10000 && Math.random()<=0.6)
 		this.hp += this.healing
 	}
 };
 
 
+
 //Characters
 let mage = new character(3000, 35, 1000, weapon[2])
 let paladin = new character(4000, 50, 600, weapon[0])
-let warlord = new character(5000, 100, 400, weapon[1])
+let warlord = new character(5000, 75, 400, weapon[1])
 
 
 
 //Enemies
-let boss = new enemy (8000, 40, 1000, 500)
-let bossTwo = new enemy (12000, 40, 2000, 650)
+let boss = new enemy (10000, 40, 1000, 500)
+let bossTwo = new enemy (15000, 40, 2000, 650)
 let bossThree = new enemy (20000, 40, 3000, 800)
 
 
 
-//Functions for buttons
+//Enemy combat
+setInterval(function(){boss.heal()}, 3000);
+setInterval(function(){boss.attack(paladin)}, 3000);
+
+
+
+//HP Updates
+setInterval(function() {
+$('#character-hp').html(paladin.hp)
+}, 100)
+setInterval(function() {
+$('#enemy-hp').html(boss.hp)
+}, 100)
+
+
+
+//Functions for combat buttons
 //---------------------------------------------------------------------
 	//Used an annonymus function to call another function because..
 	//..the attack functions are bound to the Button and return and object.
@@ -82,12 +100,12 @@ let bossThree = new enemy (20000, 40, 3000, 800)
 	$('#attack-two').click(function() {paladin.attackTwo(boss)});
 	$('#attack-three').click(function() {paladin.attackThree(boss)});
 	$('#ultimate').click(function() {paladin.ultimate(boss)}) 
-	$('#heal').click(function() {paladin.ultimate(boss)});
+	$('#heal').click(function() {paladin.heal()});
 
 
 	//Function to activate Ultimate animation
 	$('#ultimate').click(function() {
-		$('#character').toggleClass("character-attack")	 							 //toggles attack on
+		$('#character').toggleClass("character-attack")	 //toggles attack on
 		setTimeout(function() {$('#character').toggleClass("character-attack")}, 1400) //toggles attack off after 6sec.
 	});
 
@@ -96,10 +114,10 @@ let bossThree = new enemy (20000, 40, 3000, 800)
 		$('#enemy').toggleClass("enemy-attack")	 							 
 		setTimeout(function() {$('#enemy').toggleClass("enemy-attack")}, 6000) 
 	});
-//-----------------------------------------------------------------------
 
 
-//Function to choose Battleground
+
+//Thumbnails to choose Battleground
 	$('#thumbnail-waterfall').click(function () {
 		$('.screen').toggleClass("screen-background-waterfall")
 		$('.screen').toggleClass("screen-background-tree")
@@ -109,10 +127,6 @@ let bossThree = new enemy (20000, 40, 3000, 800)
 		$('.screen').toggleClass("screen-background-tree")
 		$('.screen').toggleClass("screen-background-waterfall")
 	});
-
-
-
-//Console log Batlle
 
 
 
